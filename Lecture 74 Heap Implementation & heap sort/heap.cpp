5a9f5b117340ecbfd71e4be0403331797b87431f
riazsarker <riaz.cse260@gmail.com>
2023-09-16 12:59:49 +0600

    lecture 74  heap implementation done

--- Lecture 74 Heap Implementation & heap sort/heap.cpp
+++ Lecture 74 Heap Implementation & heap sort/heap.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<queue>
 using namespace std;
 
 class heap
@@ -72,10 +73,10 @@ void heapify(int arr[],int n,int i){
     int largest=i;
     int left=2*i;
     int right=2*i+1;
-    if(left<n && arr[largest]<arr[left]){
+    if(left<=n && arr[largest]<arr[left]){
         largest =left;
     }
-    if(right<n && arr[largest]<arr[right]){
+    if(right<=n && arr[largest]<arr[right]){
         largest=right; 
     }
     if(largest !=i){
@@ -83,6 +84,14 @@ void heapify(int arr[],int n,int i){
         heapify(arr,n,largest);
     }
 
+}
+void heapSort(int arr[],int n){
+    int size=n;
+    while(size>1){
+        swap(arr[size],arr[1]);
+        size--;
+        heapify(arr,size,1);
+    }
 }
 
 int main(){
@@ -98,12 +107,36 @@ int main(){
     h.print();
     int arr[6]={-1,54,53,55,52,50};
     int n=5;
+    //heap creation
     for(int i=n/2;i>0;i--){
         heapify(arr,n,i);
     }
     cout<<"printing the array now "<<endl;
-    for(int i=1; i<n;i++){
+    for(int i=1; i<=n;i++){
         cout<< arr[i]<<" ";
     }cout<<endl;
-    return 0;
+    //heapSort
+    heapSort(arr,n);
+    cout<<"printing the sorted array now "<<endl;
+    for(int i=1; i<=n;i++){
+        cout<< arr[i]<<" ";
+    }cout<<endl;
+    cout<<"using priority queue here"<<endl;
+    //max heap
+    priority_queue<int>pq;
+    pq.push(4);
+    pq.push(2);
+    pq.push(5);
+    pq.push(3);
+    cout<<"element at top"<<pq.top()<<endl;
+    cout<<"size is"<<pq.size()<<endl;
+    //min heap
+    priority_queue<int,vector<int>,greater<int>>minheap;
+    minheap.push(4);
+    minheap.push(2);
+    minheap.push(5);
+    minheap.push(3);
+     cout<<"element at top"<<minheap.top()<<endl;
+    cout<<"size is"<<minheap.size()<<endl;
+        return 0;
 }
